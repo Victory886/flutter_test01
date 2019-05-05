fc55f36bd828ce7d62e391cd72ecea42679b739d
Terry <baosl_886@126.com>
2019-05-05 15:51:21 +0800

    导航栏上添加了几个按钮

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,23 @@ class Home extends StatelessWidget {
       backgroundColor: Colors.grey[100],
       appBar: AppBar(
         title: Text("Terry"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Navigration',
+            onPressed: () => debugPrint('search button is pressed.'),
+          ),
+          IconButton(
+            icon: Icon(Icons.more_horiz),
+            tooltip: 'Navigration',
+            onPressed: () => debugPrint('Navigration button is pressed.'),
+          ),
+        ],
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigration',
+          onPressed: () => debugPrint('Navigration button is pressed.'),
+        ),
       ),
 
       // body: ListViewDemo(),
@@ -34,4 +51,3 @@ class Home extends StatelessWidget {
     );
   }
 }
-
