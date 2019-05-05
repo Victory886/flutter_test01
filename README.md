7ea6865531dc86c6505f388832b4df50a059ce50
Terry <baosl_886@126.com>
2019-05-05 23:10:41 +0800

    抽屉页面添加listview。

--- README.md
+++ README.md
@@ -77,3 +77,50 @@ class Home extends StatelessWidget {
     ),
 
 ```
+
+- ## 本次更新主要是做了 抽屉页面显示listview
+  代码如下：
+  ```dart
+
+  class DrawerDemo extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+      return Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('header'.toUpperCase()),
+              decoration: BoxDecoration(color: Colors.grey[100]),
+            ),
+            ListTile(
+              title: Text("Messages", textAlign: TextAlign.left),
+              leading: Icon(
+                Icons.favorite,
+                color: Colors.black12,
+                size: 22.0,
+              ),
+            ),
+            ListTile(
+              title: Text("Favorite", textAlign: TextAlign.left),
+              leading: Icon(
+                Icons.message,
+                color: Colors.black12,
+                size: 22.0,
+              ),
+            ),
+            ListTile(
+              title: Text("Settings", textAlign: TextAlign.left),
+              leading: Icon(
+                Icons.settings,
+                color: Colors.black12,
+                size: 22.0,
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+  }
+
+  ```
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'demo/drawer_headerview.dart';
 // import 'model/post.dart';
 // import 'demo/bottom_navigation_bar_demo.dart';
 // import './demo/listView_demo.dart';
@@ -28,56 +29,41 @@ class Home extends StatelessWidget {
     return DefaultTabController(
       length: 3,
       child: Scaffold(
-        backgroundColor: Colors.grey[100],
-        appBar: AppBar(
-          title: Text("Terry"),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.search),
+          backgroundColor: Colors.grey[100],
+          appBar: AppBar(
+            title: Text("Hello Terry"),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.search),
+                tooltip: 'Navigration',
+                onPressed: () => debugPrint('search button is pressed.'),
+              ),
+            ],
+            leading: IconButton(
+              icon: Icon(Icons.menu),
               tooltip: 'Navigration',
-              onPressed: () => debugPrint('search button is pressed.'),
+              onPressed: () => debugPrint('Navigration button is pressed.'),
             ),
-            // IconButton(
-            //   icon: Icon(Icons.more_horiz),
-            //   tooltip: 'Navigration',
-            //   onPressed: () => debugPrint('Navigration button is pressed.'),
-            // ),
-          ],
-          leading: IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: 'Navigration',
-            onPressed: () => debugPrint('Navigration button is pressed.'),
+            bottom: TabBar(
+              unselectedLabelColor: Colors.blue,
+              indicatorColor: Colors.red,
+              indicatorWeight: 2.0,
+              tabs: <Widget>[
+                Tab(icon: Icon(Icons.local_florist)),
+                Tab(icon: Icon(Icons.change_history)),
+                Tab(icon: Icon(Icons.directions_bike)),
+              ],
+            ),
           ),
-          bottom: TabBar(
-            unselectedLabelColor: Colors.black12,
-            indicatorColor: Colors.black12,
-            indicatorWeight: 1.0,
-            tabs: <Widget>[
-              Tab(icon: Icon(Icons.local_florist)),
-              Tab(icon: Icon(Icons.change_history)),
-              Tab(icon: Icon(Icons.directions_bike)),
-            ],
-          ),
-        ),
-        
-        body: TabBarView(
-          children: <Widget>[
-            Icon(Icons.local_florist, size: 128.0, color: Colors.black12),
-            Icon(Icons.change_history, size: 128.0, color: Colors.black12),
-            Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
-          ],
-        ),
-        drawer: Container(
-          color: Colors.red,
-          padding: EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+          body: TabBarView(
             children: <Widget>[
-              Text('this is drawer')
+              Icon(Icons.local_florist, size: 128.0, color: Colors.black12),
+              Icon(Icons.change_history, size: 128.0, color: Colors.black12),
+              Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
             ],
           ),
-        ),
-      ),
+          drawer: DrawerDemo()),
     );
   }
 }
+
